3fa1a772ee83756b775e2d586ea4c8c1e6f6ba7b
Anju Priya V <84177086+anjupriya-v@users.noreply.github.com>
2022-08-11 16:46:35 +0530

    Add files via upload

---
+++ LinkedList/LinkedListInsertion.java
@@ -0,0 +1,30 @@
+class Solution
+{
+    Node insertAtBeginning(Node head, int x)
+    {
+        Node n=new Node(x);
+       if(head==null){
+           head=n;
+           return head;
+       }
+       n.next=head;
+       head=n;
+       return head;
+    }
+    
+    //Function to insert a node at the end of the linked list.
+    Node insertAtEnd(Node head, int x)
+    {
+        Node n=new Node(x);
+        if(head==null){
+            head=n;
+            return head;
+        }
+        Node temp=head;
+       while(temp.next!=null){
+           temp=temp.next;
+       }
+       temp.next=n;
+       return head;
+    }
+}
